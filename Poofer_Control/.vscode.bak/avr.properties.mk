6a54b89d1d804bc0cee42c4ce83aaf922af72b3b
Dyllan <dyllan.to@acelerex.com>
2022-02-11 21:02:58 -0500

    Poofer control updates

---
+++ Poofer_Control/.vscode.bak/avr.properties.mk
@@ -0,0 +1,4 @@
+AVR.source.compiler = C:\Users\DyllanTo\scoop\apps\avr-gcc\current\bin\avr-gcc.exe
+AVR.source.libraries = C:\Users\DyllanTo\scoop\apps\avr-gcc\current\avr\include
+AVR.device.type = atmega2560
+AVR.device.frequency = 16000000UL
